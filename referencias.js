c3bf334cb7039df1119f462299a9fd22509066a7
Airam Toscano <airamlobato@gmail.com>
2022-03-28 17:46:08 -0400

    problema com lint

---
+++ referencias.js
@@ -0,0 +1,6 @@
+// AQUI EM BAIXO ESTÁ ALGUNS SITES QUE UTILIZEI PARA OLHAR ALGUNS COMANDOS
+
+// https://stack.desenvolvedor.expert/appendix/docker/criandoimagem.html
+// https://www.macoratti.net/19/04/docker_contexec1.htm
+// https://caiodelgado.dev/dockerfile-melhores-praticas/
+// https://www.macoratti.net/19/02/dock_imgfile1.htm
